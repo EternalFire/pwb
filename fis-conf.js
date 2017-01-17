3f9975b12eae4e47a6d62274d6f0baa9fc0f4d92
EternalFire <403526076@qq.com>
2017-01-17 16:00:38 +0800

    add glob string

--- fis-conf.js
+++ fis-conf.js
@@ -1,6 +1,27 @@
+
 fis.hook('commonjs', {
-  // 配置项
+  extList: ['.js', '.jsx']
 });
+
+fis.match('/module/(**)/*.js', {
+  packTo: '/static/$1.js'
+});
+
+fis.match('{/module/**.js,/view/*.jsx}', {
+  parser: fis.plugin('babel-6.x', {
+    sourceMaps: true
+  }),
+  rExt: '.js',  
+  isMod: true
+});
+
+fis.match('/test/**', {
+  release: false
+});
+
+// fis.match('*.{js,jsx}', {
+//   optimizer: fis.plugin('uglify-js')
+// });
 
 fis.match('::package', {
   // npm install [-g] fis3-postpackager-loader
@@ -10,25 +31,3 @@ fis.match('::package', {
     useInlineMap: true // 资源映射表内嵌
   })
 });
-
-fis.match('/module/**/*.js', {
-  isMod: true
-});
-
-fis.match('/module/(**)/*.js', {
-  packTo: '/static/$1.js'
-});
-
-// 编译所有后缀为 jsx 的文件为 js
-fis.match('{*.jsx,*:jsx}', {
-    parser: fis.plugin('babel-6.x', {
-        sourceMaps: true
-    }),
-    rExt: '.js'
-});
-
-
-fis.match('/test/**', {
-  release: false
-});
-
