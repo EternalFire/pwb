fe9006da86dfe471139d879630085a65c15c64e1
EternalFire <403526076@qq.com>
2017-01-23 15:25:25 +0800

    ToolbarItemToggle support 'data-*'

--- view/ToolbarItemToggle.jsx
+++ view/ToolbarItemToggle.jsx
@@ -8,6 +8,28 @@ class ToolbarItemToggle extends React.Component {
     };
   }
 
+  componentDidMount() {
+    console.log('ToolbarItemToggle', this.props);
+
+    Object.keys(this.props)
+      .filter((key) => {
+        return key.indexOf('data-') === 0;
+      })
+      .map((key) => {
+        let m = { src: key, dst: ''};
+
+        m.dst = key.split('-').splice(1).reduce((a, b) => {
+          a = a + b[0].toUpperCase() + b.substr(1)
+          return a;  
+        });
+
+        return m;
+      })
+      .forEach((keyMap) => {
+        this.refs['btn'].dataset[keyMap.dst] = this.props[keyMap.src];
+      });
+  }
+
   handleClick() {
     let isOn = !this.state.isOn;
 
@@ -24,7 +46,11 @@ class ToolbarItemToggle extends React.Component {
     const { icon, text } = this.props;
 
     return (
-      <button className="btn btn-default btn-lg" onClick={() => this.handleClick()} ref="btn">
+      <button className="btn btn-default btn-lg" 
+        onClick={() => this.handleClick()} 
+        ref="btn" 
+
+      >
         <span className={icon}></span> {text}
       </button>      
     )
